30508ad6e7161fb935557e68f6c9e93a1c5df1b5
emeal333 <106497339+emeal333@users.noreply.github.com>
2023-10-23 19:41:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import button
 
 pygame.init()
 
@@ -25,11 +26,19 @@ image_x = screen.get_width() // 2 - img_wdth // 2
 image_y = window_height // 2 - img_hght
 
 
+
+buttons = pygame.image.load('buttons.png').convert_alpha()
+
+
+resume_button_rect = pygame.Rect(30,220, 220, 100)
+resume_button = buttons.subsurface(resume_button_rect)
+# resume_button = button.Button(50, 50, buttons, 1)
+#
 # camera_x = 0
 # camera_speed = 1
 camera_offset_x = 0
 
-movement_speed = 1
+movement_speed = .5
 
 w = (255, 255, 255)
 b = (0, 0, 0)
@@ -52,8 +61,10 @@ dot_y = 225
 #             pygame.quit()
 
 
-rectangle = pygame.Rect(50,50,50,50)
+rectangle = pygame.Rect((screen.get_width()),(window_height // 2) - img_hght,50,50)
 
+
+black_square = pygame.Rect((screen.get_width() //2, window_height //2, 50, 50))
 
 def crash():
     global running
@@ -67,7 +78,7 @@ def crash():
         running = False
 
 running = True
-
+game_started = False
 
 
 while running:
@@ -75,47 +86,57 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    keys = pygame.key.get_pressed()
+        if not game_started and event.type == pygame.MOUSEBUTTONDOWN and resume_button_rect.collidepoint(event.pos):
+            game_started = True
+
+    # keys = pygame.key.get_pressed()
 
 
+    if game_started:
+        keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and image_x - movement_speed > 0:
-        image_x -= movement_speed
-        # dot_x -= movement_speed
-    if keys[pygame.K_RIGHT] and image_x + movement_speed < screen.get_width():
+    # if keys[pygame.K_LEFT] and image_x - movement_speed > 0:
+    #     image_x -= movement_speed
+    #     # dot_x -= movement_speed
+    # if keys[pygame.K_RIGHT] and image_x + movement_speed < screen.get_width():
+    #     image_x += movement_speed
+    #     # dot_x += movement_speed
+        if keys[pygame.K_UP] and image_y - movement_speed > 0:
+            image_y -= movement_speed
+        if keys[pygame.K_DOWN] and image_y + movement_speed < (window_height //2) - 50:
+            image_y += movement_speed
+
         image_x += movement_speed
-        # dot_x += movement_speed
-    if keys[pygame.K_UP] and image_y - movement_speed > 0:
-        image_y -= movement_speed
-    if keys[pygame.K_DOWN] and image_y + movement_speed < (window_height //2) - 50:
-        image_y += movement_speed
-
     # distance = ((image_x - dot_x) ** 2 + (image_y - dot_y) ** 2) ** 0.5
     # if distance < dot_radius:
     #     running = False
 
-    camera_offset_x = screen.get_width() // 2 - image_x - img_wdth // 2
+        camera_offset_x = screen.get_width() // 2 - image_x - img_wdth // 2
 
 
     # pygame.draw.circle(screen, b, (dot_x - camera_offset_x, dot_y), dot_radius)
     # dot = pygame.draw.circle(screen,200,100,100)
 # dot_pos =
 #     rectangle = pygame.Rect(50,50,50,50)
-    rect_draw_pos = rectangle.move(camera_offset_x, 0)
+        rect_draw_pos = rectangle.move(camera_offset_x, 0)
 
-    screen.fill(w)
+        screen.fill(w)
 
-    pygame.draw.line(screen, b, (line_x1, line_y1), (line_x2, line_y2), 2)
+        pygame.draw.line(screen, b, (line_x1, line_y1), (line_x2, line_y2), 2)
 
-    pygame.draw.rect(screen, (255, 0, 0), rect_draw_pos)
+        pygame.draw.rect(screen, (255, 0, 0), rect_draw_pos)
 
-    screen.blit(duck, (image_x + camera_offset_x, image_y))
+        screen.blit(duck, (image_x + camera_offset_x, image_y))
 
-    duck_rect = duck.get_rect().move(image_x + camera_offset_x, image_y)
-    if duck_rect.colliderect(rect_draw_pos):
-        running = False
+        duck_rect = duck.get_rect().move(image_x + camera_offset_x, image_y)
+        if duck_rect.colliderect(rect_draw_pos):
+            running = False
     # if duck.get_rect().move(image_x, image_y).colliderect(rect_draw_pos):
     #     running = False
+
+    else:
+        screen.fill(w)
+        screen.blit(resume_button, (40,200))
 
     pygame.display.update()
 
